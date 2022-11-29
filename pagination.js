b633efa2c8d010530ddff0dbe407b758d3f468e1
Sean Dylan Patterson <74496368+SeanDylan1982@users.noreply.github.com>
2022-11-29 08:35:41 +0200

    Add files via upload

--- pagination.js
+++ pagination.js
@@ -96,3 +96,26 @@ const setCurrentPage = (pageNum) => {
     }
   });
 };
+
+window.addEventListener("load", () => {
+  getPaginationNumbers();
+  setCurrentPage(1);
+  
+  prevButton.addEventListener("click", () => {
+    setCurrentPage(currentPage - 1);
+  });
+  
+  nextButton.addEventListener("click", () => {
+    setCurrentPage(currentPage + 1);
+  });
+  
+  document.querySelectorAll(".pagination-number").forEach((button) => {
+    const pageIndex = Number(button.getAttribute("page-index"));
+  
+    if (pageIndex) {
+      button.addEventListener("click", () => {
+        setCurrentPage(pageIndex);
+      });
+    }
+  });
+});
